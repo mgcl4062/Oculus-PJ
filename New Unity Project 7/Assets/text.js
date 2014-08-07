bb6b06ba72bc74b3ac8ccddf0c622cb1b86273a6
Koma <cnn.mgcl.4062@gmail.com>
2014-08-07 22:04:23 +0900

    first commit

---
+++ New Unity Project 7/Assets/text.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+//GUITextにtxtファイルから文字列を読み込んで表示する例
+//このスクリプトをGUITextに貼り付けて使用する
+//Resourcesフォルダに配置したtxtファイルである "testScript.txt"を読み込む
+
+function Start () {
+	var txt : TextAsset =Instantiate(Resources.Load("testScript", TextAsset));
+	guiText.text = txt.text;
+}
